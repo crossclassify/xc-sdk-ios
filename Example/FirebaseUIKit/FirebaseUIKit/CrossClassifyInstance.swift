f236ff407ae58551035f87d92e678334f293dd9f
Ryan BM <aryanbahmaani@gmail.com>
2023-03-02 16:36:37 +0330

    [add] read SITE_ID and API_KEY from env var

--- Example/FirebaseUIKit/FirebaseUIKit/CrossClassifyInstance.swift
+++ Example/FirebaseUIKit/FirebaseUIKit/CrossClassifyInstance.swift
@@ -1,5 +1,9 @@
 import CrossClassify
 
 extension CrossClassify {
-    public static let shared = CrossClassify(siteId: "1", apiKey: "GOTOappDOTcrossclassifyDOTcom")
+    public static var shared: CrossClassify {
+        let siteId = ProcessInfo.processInfo.environment["SITE_ID"] ?? ""
+        let apiKey = ProcessInfo.processInfo.environment["API_KEY"] ?? ""
+        return CrossClassify(siteId: siteId, apiKey: apiKey)
+    }
 }
